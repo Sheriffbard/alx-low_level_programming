b594206ac193b42cf43b929ed264bc4fa733cc1d
Sheriffbard <olawuyiola@gmail.com>
2022-09-08 14:23:17 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 
 /**
- *  * main - Entry point
- *   * Description: prints "\"Programming is like building a multilingual puzzle"
- *    * Return: 0
- *     */
+  * main - Entry point
+  * Description: prints "\"Programming is like building a multilingual puzzle"
+    * Return: 0
+    */
 
 int main(void)
 {
