88e2a6d67bffb48c0cbddd599d6cea4d57b18018
Sheriffbard <olawuyiola@gmail.com>
2022-09-13 14:47:06 -0700

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,11 +1,11 @@
-#include <main.h>
+#include "main.h"
 
 /**
  * main - Entry point
  * Description: prints the alphabet, in lowercase, followed by a new line
  * Return: Always (0) Success
  */
-int main(void)
+void print_alphabet(void)
 {
 	char ch;
 
