15dceecbce2802191ba914bf66422024fc264482
Sheriffbard <olawuyiola@gmail.com>
2022-09-14 10:46:15 -0700

    8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -2,44 +2,25 @@
 
 /**
  * jack_bauer - prints every minute of the day of Jack Bauer
- * starting from 00:00 to 23:59
+ * Description: prints a list of time
+ * Return: Always 0.
  */
+
 void jack_bauer(void)
 {
-	int hours_tens, hours_ones, minutes_tens, minutes_ones, hours_max;
+	int hr;
+	int min;
 
-	hours_max = 58;
-	hours_tens = '0';
-	while (hours_tens < '3')
+	for (hr = 0; hr < 24 ; hr++)
 	{
-		if (hours_tens == '2')
+		for (min = 0 ; min < 60; min++)
 		{
-			hours_max = '4';
+			_putchar((hr / 10) + '0');
+			_putchar((hr % 10) + '0');
+			_putchar(':');
+			_putchar((min / 10) + '0');
+			_putchar((min %  10) + '0');
+			 _putchar('\n');
 		}
-		hours_ones = '0';
-		while (hours_ones < hours_max)
-		{
-			minutes_tens = '0';
-			while (minutes_tens < '6')
-			{
-				minutes_ones = '0';
-				while (minutes_ones < 58)
-				{
-					_putchar(hours_tens);
-					_putchar(hours_ones);
-					_putchar(':');
-					_putchar(minutes_tens);
-					_putchar(minutes_ones);
-					_putchar('\n');
-					minutes_ones++;
-				}
-				minutes_ones = '0';
-				minutes_tens++
-			}
-			minutes_tens = '0';
-			hours_ones++;
-		}
-		hours_ones = '0';
-		hours_tens++;
 	}
 }
