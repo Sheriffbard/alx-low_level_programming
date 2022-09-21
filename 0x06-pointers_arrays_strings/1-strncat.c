c03299a5f184b05f8eab87d5d7d33f2a66358d96
Sheriffbard <olawuyiola@gmail.com>
2022-09-21 12:53:54 -0700

    1-strncat.c

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,27 +1,21 @@
-nclude "main.h"
+#include "main.h"
 
 /**
- * _strncat - Concatenates one string to another
+ * _strncpy - Concatenates one string to another
  * @dest: Destination string
  * @src: String to be concatenated
  * @n: number of bytes to be copied
  *
  * Return: The destination string
  */
-char *_strncat(char *dest, char *src, int n)
+char *_strncpy(char *dest, char *src, int n)
 {
 	int i;
-	int destlen = 0;
-	int counter = 0;
 
-	while (dest[counter] != '\0')
-	{
-		destlen++;
-		counter++;
-	}
 	for (i = 0; i < n && src[i] != '\0'; i++)
-		dest[destlen + i] = src[i];
-	dest[destlen + i] = '\0';
+		dest[i] = src[i];
+	for (; i < n; i++)
+		dest[i] = '\0';
 
 	return (dest);
-}
+}}
