d9f7a5a777af66b745cdcc38c21c0cf7458f4b2c
Sheriffbard <olawuyiola@gmail.com>
2022-10-04 05:50:07 -0700

    Task 0 static library libmy.a

---
+++ 0x09-static_libraries/2-strlen.c
@@ -0,0 +1,13 @@
+#include <string.h>
+#include "main.h"
+
+/**
+ * _strlen - prints the length of a string
+ * @s: string variable
+ *
+ * Return: length of the string
+ */
+int _strlen(char *s)
+{
+	return (strlen(s));
+}
