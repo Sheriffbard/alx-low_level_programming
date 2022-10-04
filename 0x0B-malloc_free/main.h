b3cc23f88c7a2fec0b12fe732f9f2d30684fcc19
Sheriffbard <olawuyiola@gmail.com>
2022-10-04 12:45:44 -0700

    malloc

---
+++ 0x0B-malloc_free/main.h
@@ -0,0 +1,7 @@
+char **strtow(char *);
+char *argstostr(int, char **);
+void free_grid(int **grid, int row);
+int **alloc_grid(int col, int row);
+char *str_concat(char *s1, char *s2);
+char *_strdup(char *str);
+char *create_array(unsigned int size, char c);
